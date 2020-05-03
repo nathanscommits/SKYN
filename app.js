c96c176898aa9d025b8fd04520d7cef377411e77
Nathan <contact@mindbodyeducation.com>
2020-05-03 12:12:46 +1000

    8

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
   
 app.post('/endpoint', (req, res) => {
-    console.log(req.body.todo)
+    console.log(req.body)
 })
 app.get('/', function (req, res){
     res.send('home page!')
