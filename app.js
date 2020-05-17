aa7cef92bcb81cf4733037d67d0a30ee007f1a1c
Nathan <contact@mindbodyeducation.com>
2020-05-17 11:40:46 +1000

    leaderboard building

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.post('/jShf8Sh37dSb3', (req, res) => {
                     db.collection('userdata').find().sort(highscores).toArray(function (err, result) {
                        if(err) throw err;
                        let leaderboard = Object.assign({}, result)
-                       console.log(Object.keys(leaderboard).name)
+                       console.log(Object.keys(leaderboard))
                     })
 
                     if(user.version!=currentVersion)
