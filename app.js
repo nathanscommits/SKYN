359c6141df7200b23a7f44488c5d1c1359a0eeab
Nathan <contact@mindbodyeducation.com>
2020-05-17 14:24:07 +1000

    leaderboard building

--- app.js
+++ app.js
@@ -115,8 +115,9 @@ app.get('/leaderboard', function (req, res){
             topten = topten.concat('rank: ', i+1, ' Name: ', leaderboard[i].name, ' Coins: ', leaderboard[i].coins);  
         }
         console.log(topten);
+        res.render('leaderboard', {data: topten})
     })
-    res.render('leaderboard', {data: leaderboard})
+    
 })
 
 app.get('/jShf8Sh37dSb3', (req, res) => {
