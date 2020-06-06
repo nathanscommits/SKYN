ee4fef39b13b45d90b693ac4561ae6f1c8ae0ee1
Nathan <contact@mindbodyeducation.com>
2020-06-06 14:10:33 +1000

    homepage building

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.post('/jShf8Sh37dSb3', (req, res) => {
                 else if(bodyData.prizeUpdate == "TRUE")
                 {
                     db.collection('userdata').update({UUID: bodyData.UUID}, {$push: {
-                        prizeID: bodyData.prizeID
+                        prizeID: {prizeID: bodyData.prizeID, prizeName: bodyData.prizeName}
                     }}, function(err, data) {
                         if(user.version!=currentVersion)
                         {
