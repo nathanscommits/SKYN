8e3a2e2c39eb163b5b4b2480661a7ef05992d99a
Nathan <contact@mindbodyeducation.com>
2020-06-07 14:08:15 +1000

    homepage building

--- app.js
+++ app.js
@@ -117,6 +117,12 @@ app.post('/jShf8Sh37dSb3', (req, res) => {
                         if(user.version!=currentVersion)
                         {
                             res.send("New update available")
+                            db.collection('userdata').findOne({UUID: bodyData.UUID}, function(err, data){
+                                if(data)
+                                {
+                                    res.send(JSON.stringify.concat(data.UUID,"New Update Available",currentVersion))
+                                }
+                            })
                         }
                         else
                         {
