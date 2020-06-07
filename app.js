4ec1f9c0acaad181d13c9d80acebdac3455c4243
Nathan <contact@mindbodyeducation.com>
2020-06-07 20:49:05 +1000

    homepage building

--- app.js
+++ app.js
@@ -80,57 +80,60 @@ app.post('/jShf8Sh37dSb3', (req, res) => {
                     })
                 }
                 else
-                {
-                    db.collection('userdata').findOneAndUpdate({UUID: bodyData.UUID}, {$set: {
-                        //UUID: bodyData.UUID,
-                        version: bodyData.version,
-                        name: bodyData.name,
-                        coins: bodyData.coins,
-                        fitness: bodyData.fitness,
-                        hair: bodyData.hair,
-                        fat: bodyData.fat,
-                        tan: bodyData.tan,
-                        voice: bodyData.voice,
-                        temp: bodyData.temp,
-                        features: bodyData.features,
-                        timeAlive: bodyData.timeAlive,
-                        cItems: bodyData.cItems,
-                        cDumbbells: bodyData.cDumbbells,
-                        thirst: bodyData.thirst,
-                        calories: bodyData.calories,
-                        sleep: bodyData.sleep,
-                        health: bodyData.health,
-                        slotPlays: bodyData.slotPlays,
-                        totalSlots: bodyData.totalSlots,
-                        totalCoins: bodyData.totalCoins,
-                        totalSpent: bodyData.totalSpent,
-                        deathCount: bodyData.deathCount
-                    }}, function(err, data) {
-
-                        /*let highscores = { coins: -1 }
-                        db.collection('userdata').find().sort(highscores).toArray(function (err, result) {
-                        if(err) throw err;
-                        let leaderboard = Object.assign({}, result)
-                        console.log('Name: ', leaderboard[0].name, ' Coins: ', leaderboard[0].coins)
-                        console.log('new ranking')
-                        }) */
-
-                        if(user.version!=currentVersion)
-                        {
-                            //res.send("New update available")
-                            db.collection('userdata').findOne({UUID: bodyData.UUID}, function(err, data){
-                                if(data)
-                                {
-                                    sendstring = data.UUID + "New Update Available" + currentVersion
-                                    res.send(sendstring) //data.UUID,"New Update Available",currentVersion
-                                }
-                            })
-                        }
-                        else
-                        {
-                            res.send("New update made")
-                        }
-                    })
+                { 
+                    if(bodyData.version==currentVersion)
+                    {
+                        db.collection('userdata').findOneAndUpdate({UUID: bodyData.UUID}, {$set: {
+                            //UUID: bodyData.UUID,
+                            version: bodyData.version,
+                            name: bodyData.name,
+                            coins: bodyData.coins,
+                            fitness: bodyData.fitness,
+                            hair: bodyData.hair,
+                            fat: bodyData.fat,
+                            tan: bodyData.tan,
+                            voice: bodyData.voice,
+                            temp: bodyData.temp,
+                            features: bodyData.features,
+                            timeAlive: bodyData.timeAlive,
+                            cItems: bodyData.cItems,
+                            cDumbbells: bodyData.cDumbbells,
+                            thirst: bodyData.thirst,
+                            calories: bodyData.calories,
+                            sleep: bodyData.sleep,
+                            health: bodyData.health,
+                            slotPlays: bodyData.slotPlays,
+                            totalSlots: bodyData.totalSlots,
+                            totalCoins: bodyData.totalCoins,
+                            totalSpent: bodyData.totalSpent,
+                            deathCount: bodyData.deathCount
+                        }}, function(err, data) {
+    
+                            /*let highscores = { coins: -1 }
+                            db.collection('userdata').find().sort(highscores).toArray(function (err, result) {
+                            if(err) throw err;
+                            let leaderboard = Object.assign({}, result)
+                            console.log('Name: ', leaderboard[0].name, ' Coins: ', leaderboard[0].coins)
+                            console.log('new ranking')
+                            }) */
+    
+                            if(user.version!=currentVersion)
+                            {
+                                //res.send("New update available")
+                                db.collection('userdata').findOne({UUID: bodyData.UUID}, function(err, data){
+                                    if(data)
+                                    {
+                                        sendstring = data.UUID + "New Update Available" + currentVersion
+                                        res.send(sendstring) //data.UUID,"New Update Available",currentVersion
+                                    }
+                                })
+                            }
+                            else
+                            {
+                                res.send("New update made")
+                            }
+                        })
+                    }
                 }
             }
             else{
