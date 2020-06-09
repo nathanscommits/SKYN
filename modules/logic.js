c55d11e10a473833355d2d7d9744b5d65079dcfc
Nathan <nathnbrns@gmail.com>
2020-06-09 20:09:29 +1000

    a

--- modules/logic.js
+++ modules/logic.js
@@ -1,95 +1,150 @@
-function decrypt(x,y,z)
-{
-    let a = x.indexOf(y) + y.length()
-    let b = x.indexOf(z) - 1
-    if(z==-1) b = -1
-    if(y==0) a=0
-    return x.substring(a,b)
-}
 
-exports.values = function(userdata, body) {
-    let sleep = userdata.sleep
-    let energy = userdata.energy
-    let fitness = userdata.fitness
-
-    let consumed = body.consumed
-    consumed.type = decypt(consumed, "type:", "ammount:")
-    consumed.ammount = decrypt(consumed, "ammount:", "effect:")
-    consumed.effect = decrypt(consumed, "effect:", "fat:")
-    consumed.fat = decrypt(consumed, "fat:", -1)
+exports.values = function(ud, body) {
+    let consumed = JSON.parse(body.consumed)
 
     let action = body.action
     if(action.substring(0, 0)==1) //sitting
     {
-        energy+=(fitness/50); //resting
+        ud.energy+=(ud.fitness/50); //resting
 
-        if(action.substring(5, 5)>=1) //sleeping on ground =1 on object =2
+        if(action.substring(5, 5)>=1) //ud.sleeping on ground =1 on object =2
         {
-            sleep--;
-            energy+=(fitness/20); //resting even more
+            ud.sleep--;
+            ud.energy+=(ud.fitness/20); //resting even more
+
+            if(action.substring(5,5)==1) //ground sleeping
+            {
+                response+= {
+                    anim: "sleeping",
+                    sound: "snoring"
+                }
+            }
+            else //chair sleeping
+            {
+                response+= {
+                    anim: "sleeping_chair",
+                    sound: "snoring"
+                }
+            }
+            
         }
-        else //sitting but not sleeping
+        else //sitting but not ud.sleeping
         {
-            sleep+=0.05; //get sleepy if not sleeping while sitting
+            ud.sleep+=0.05; //get ud.sleepy if not ud.sleeping while sitting
         }
     }
     else{ // not sitting
-        sleep+=0.1; //not sitting, get sleepy! you could do 1/energy to make it based off of energy loss
+        ud.sleep+=0.1; //not sitting, get ud.sleepy! you could do 1/ud.energy to make it based off of ud.energy loss
     }
     
     if(action.substring(1, 1)==1) //flying
     {
-        energy-=4;
+        ud.energy-=4;
     }
     if(action.substring(2, 2)==1) //running
     {
-        energy-=2;
+        ud.energy-=2;
     }
     if(action.substring(3, 3)==1) //walking/running
     {
-        energy--;
+        ud.energy--;
     }
     if(action.substring(4, 4)==1) //jumping
     {
-        energy-=4;
+        ud.energy-=4;
+    }
+    if(energy<=0){
+        response+= {
+            anim: "exhausted",
+            sound: "breathing"
+        }
     }
     if(action.substring(0,4)=="00000") //standing
     {
-        energy+=(fitness/100); //regain energy while idle
-        fitness-=0.01; //passive fitness loss
+        ud.energy+=(ud.fitness/100); //regain ud.energy while idle
+        ud.fitness-=0.01; //passive ud.fitness loss
     }
-    if(energy<fitness)
+    if(ud.energy<ud.fitness)
     {
-        fitness+=100/fitness; //fitness gain when exercising, more fitness means harder to earn fitness
+        ud.fitness+=100/ud.fitness; //ud.fitness gain when exercising, more ud.fitness means harder to earn ud.fitness
     }
 
     //if(action.substring(5, 5)==1) //underwater
 
     //if(action.substring(6, 6)==1) //in sun
 
-    if(consumed.type == "food") //eating something
+    //consumables
+
+    if(consumed.type != "food") 
     {
-        hunger+=consumed.ammount
-        weight += consumed.fat
+        ud.hunger-=0.1; //ud.hunger loss when not eating
+        ud.fat-=0.1; //ud.fat loss when not eating
+        ud.pimples-=0.1;
     }
-    else 
+    if(consumed.type != "drink")
     {
-        hunger-=0.1; //hunger loss when not eating
+        ud.thirst-=0.1; //ud.thirst loss when not drinking
+    }
+    
+    if(consumed) //consumed effects
+    {
+        if(consumed.hunger) ud.hunger+=consumed.hunger
+        if(consumed.fat) ud.fat += consumed.fat
+        if(consumed.pimples) ud.pimples+=consumed.pimples
+        if(consumed.energy) ud.energy+=consumed.energy
+        if(consumed.sleep) ud.sleep+=consumed.sleep
+        if(consumed.thirst) ud.thirst+=consumed.thirst
+        if(consumed.health) ud.health+=consumed.health
     }
 
-    if(consumed.type == "drink")
+    //ud.health
+    if(ud.hunger>=100) ud.health-=0.5
+    if(ud.thirst>=100) ud.health-=0.5
+    else if(ud.hunger<100) ud.health+=0.1 //passive ud.health regain
+
+    //value min/max's
+    if(ud.energy<0) ud.energy=0;
+    if(ud.energy>ud.fitness) ud.energy=ud.fitness;
+    if(ud.sleep>100) ud.sleep=100;
+    if(ud.sleep<0) ud.sleep=0;
+    if(ud.fitness<0) ud.fitness=0;
+    if(ud.hunger>100) ud.hunger=100;
+    if(ud.hunger<0) ud.hunger=0;
+    if(ud.thirst>100) ud.thirst=100
+    if(ud.thirst<0) ud.thirst=0;
+    if(ud.pimples>100) ud.pimples=100;
+    if(ud.pimples<0) ud.pimples=0;
+    if(ud.fat>100) ud.fat=100;
+    if(ud.fat<0) ud.fat=0;
+    if(ud.health>100) ud.health=100;
+    if(ud.health<0) ud.health=0;
+    if(ud.coins<0) ud.coins=0;
+
+    //ud.coins
+    if(ud.health>0)
     {
-        thirst+=consumed.ammount
+        ud.timeAlive++;
+        if(ud.hunger>=100) ud.coins+=0.01;
+        if(ud.thirst>=100) ud.coins+=0.01;
+        if(ud.fitness>=100) ud.coins+=ud.fitness/10000;
+        if(ud.energy<ud.fitness) ud.coins+=ud.fitness-ud.energy/100000;
+        if(ud.sleep<100) ud.coins+=100-ud.sleep/10000;
     }
-    else
+    else //dieing
     {
-        thirst-=0.1;
+        ud.deathCount++
+        ud.timeAlive = 0;
+        ud.coins-= (ud.coins/10)
+        ud.health = 100
+        ud.hunger=0
+        ud.thirst=0
+        ud.sleep=0
+        response+= {
+            osay: "You died!",
+            anim: "death",
+            sound: "dieing"
+        }
     }
 
-
-
-    if(sleep<0) sleep=0;
-    if(energy<0) energy=0;
-    if(fitness<0) fitness=0;
-    //if(fitness>1000) fitness=1000; //fitness cap
+    //if(ud.fitness>1000) ud.fitness=1000; //ud.fitness cap
 }
