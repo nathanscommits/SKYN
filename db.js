bd354c9bde6ee31d367cbe648b8d0af380dcfefb
Nathan <nathnbrns@gmail.com>
2020-06-09 18:44:47 +1000

    a

--- db.js
+++ db.js
@@ -5,10 +5,5 @@ let connectionString = 'mongodb+srv://sharky:L293nShoTQPODgLi@cluster0-xivcd.gcp
 mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client){
     module.exports = client.db().collection('userdata')
     const app = require("./app")
-    app.listen(3000)
+    app.listen(process.env.PORT || 3000)
 })
-
-let port = process.env.PORT
-if(port == null || port == '') {
-  port = 3000
-}
