613f36f8c9826839c06f283e7e56dae0f906f333
Nathan <nathnbrns@gmail.com>
2020-06-19 09:07:23 +1000

    a

--- modules/anims.js
+++ modules/anims.js
@@ -1,21 +1,23 @@
-exports.sleeps = [
+anims.sleeps = [
     "Sleep_01_anim",
     "Sleep_02_anim",
     "Sleep_03_anim",
     "Sleep_04_anim",
 ]
-exports.death = "death"
-exports.csleeps = "Sleeping_Chair_loop"
-exports.stretch = "Stretch"
-exports.exhausted = "Exhausted_WIP"
-exports.standsleep = "drowsyStanding_WIP_noloop"
-exports.breathing = "HeavyBreathing_WIP_noloop"
-exports.fanning = [
+anims.death = "death"
+anims.csleeps = "Sleeping_Chair_loop"
+anims.stretch = "Stretch"
+anims.exhausted = "Exhausted_WIP"
+anims.standsleep = "drowsyStanding_WIP_noloop"
+anims.breathing = "HeavyBreathing_WIP_noloop"
+anims.fanning = [
     "Hot_Fanning_One_WIP_noloop",
     "Hot_Fanning_One_INVERT_noloop",
     "Hot_Fanning_Both_WIP_noloop",
     "Hot_Fanning_Both_Version2_noloop",
     "Hot_Fanning_Both_NoSync"
 ]
-exports.sweatwipe = "Sweat_Wipe_WIP_noloop"
-exports.hungry = "Hungry_WIP_noloop"
+anims.sweatwipe = "Sweat_Wipe_WIP_noloop"
+anims.hungry = "Hungry_WIP_noloop"
+
+module.exports = anims
