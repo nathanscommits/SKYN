e4bb4e16d809498f4626a926eb563e26c2989888
Nathan <nathnbrns@gmail.com>
2020-06-19 09:18:26 +1000

    a

--- modules/logic3.js
+++ modules/logic3.js
@@ -1,5 +1,4 @@
 
-let anims = require("anims")
 let playsound = require("sounds")
 
 exports.values = function(body) {
@@ -263,3 +262,25 @@ exports.values = function(body) {
 
     body.response.queue = queue
 }
+let anims = {}
+anims.sleeps = [
+    "Sleep_01_anim",
+    "Sleep_02_anim",
+    "Sleep_03_anim",
+    "Sleep_04_anim",
+]
+anims.death = "death"
+anims.csleeps = "Sleeping_Chair_loop"
+anims.stretch = "Stretch"
+anims.exhausted = "Exhausted_WIP"
+anims.standsleep = "drowsyStanding_WIP_noloop"
+anims.breathing = "HeavyBreathing_WIP_noloop"
+anims.fanning = [
+    "Hot_Fanning_One_WIP_noloop",
+    "Hot_Fanning_One_INVERT_noloop",
+    "Hot_Fanning_Both_WIP_noloop",
+    "Hot_Fanning_Both_Version2_noloop",
+    "Hot_Fanning_Both_NoSync"
+]
+anims.sweatwipe = "Sweat_Wipe_WIP_noloop"
+anims.hungry = "Hungry_WIP_noloop"
