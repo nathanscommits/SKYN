79dc6fb5957c4dbca2ce4c28edcabdd3fcc7d64e
Nathan <nathnbrns@gmail.com>
2020-06-19 10:17:57 +1000

    a

--- controllers/postController2.js
+++ controllers/postController2.js
@@ -1,7 +1,7 @@
 const db = require('../db')
 const logic = require('../modules/logic3')
 const pool = require('../collections/prizes')
-const build = "0.10"
+const build = "0.11"
 let body = {}
 exports.hudUpdate = function (req, res) {
     
