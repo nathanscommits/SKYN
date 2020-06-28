8cc86f5648ae98d0b138c2eb1d497873175376a3
Nathan <nathnbrns@gmail.com>
2020-06-28 15:29:31 +1000

    a

--- controllers/postController2.js
+++ controllers/postController2.js
@@ -11,10 +11,10 @@ exports.hudUpdate = function (req, res) {
         info:{
             listen: req.body.listen,
             objects:{
-                o_names: req.body.o_names.split(", "),
-                o_bb: req.body.o_bb.split(", "),
-                o_range: req.body.o_range.split(", "),
-                o_pos: req.body.o_pos.split(", ")
+                o_names: req.body.o_names.split(",&,"),
+                o_bb: req.body.o_bb.split(",&,"),
+                o_range: req.body.o_range.split(",&,"),
+                o_pos: req.body.o_pos.split(",&,")
             },
             voice: req.body.voice,
             features: req.body.features,
