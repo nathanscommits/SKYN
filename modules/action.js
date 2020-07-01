4b704c8acf50fc2120cd76fa57242d29b6aa5247
Nathan <nathnbrns@gmail.com>
2020-07-01 11:10:30 +1000

    a

--- modules/action.js
+++ modules/action.js
@@ -66,9 +66,12 @@ module.exports = function(body) {
         body.values.tan-=0.01
     }
 
-    if(body.info.wet_object != "" || body.info.water > body.info.pos.split(",")[2]-0.5) { //body.info.submerged == 1
+    let poz = body.info.pos.split(",")
+    poz.replace(">", "")
+    poz.replace("<", "")
+    if(body.info.wet_object != "" || body.info.water > poz[2]-0.5) { //body.info.submerged == 1
         body.states.wet = 120
-        if(body.info.features.substring(5,6)=="1" && body.info.water > body.info.pos.split(",")[2]+1)body.values.oxygen -= 1
+        if(body.info.features.substring(5,6)=="1" && body.info.water > poz[2]+1)body.values.oxygen -= 1
     } else {
         body.values.oxygen += 5
     }
