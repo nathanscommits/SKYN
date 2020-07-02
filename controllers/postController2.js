6263ec5756f000e6ebdb28f28d71fc172032e49a
Nathan <nathnbrns@gmail.com>
2020-07-02 11:58:26 +1000

    a

--- controllers/postController2.js
+++ controllers/postController2.js
@@ -90,7 +90,7 @@ exports.hudUpdate = function (req, res) {
             )
 
                 .then(function(data){
-                    //console.log(data)
+                    console.log(data.response.version)
                     //if(!data.response.version) reject(console.log("no data to process"))
                     if(data.response.version == build) {
                         body.values = data.values
