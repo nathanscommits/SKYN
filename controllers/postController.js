c6d51412f5b1928a1427ad8c5b76aa2fd8d3ab54
Nathan <nathnbrns@gmail.com>
2020-07-03 10:16:46 +1000

    a

--- controllers/postController.js
+++ controllers/postController.js
@@ -318,7 +318,7 @@ exports.refund = function(req, res) {
         else
         {
             db.findOneAndUpdate({UUID: req.body.UUID}, {$set: {
-                "values.coins": (ud.coins + i)
+                "values.coins": (ud.values.coins + i)
             }}, function(err, data) {
                 res.send("Coins refunded.")
             })
