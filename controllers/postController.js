cf91e224f67c4576e66a1a80f7c6c0004b14a667
Nathan <nathnbrns@gmail.com>
2020-07-03 11:34:29 +1000

    a

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,10 +2,10 @@ const db = require('../db')
 const logic = require('../modules/logic')
 const pool = require('../collections/prizes')
 
-const build = "0.12"
+const build = "0.12.1"
 
 exports.hudUpdate = function (req, res) {
-    
+    console.log(req.body.name)
     let body = {
         UUID: req.body.UUID,
         name: req.body.name,
